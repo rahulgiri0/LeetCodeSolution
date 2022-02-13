5f6a2f1fac0ff137ba88ca5c4b64bd85c76e81c8
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-02-13 12:15:40 +0530

    Time: 236 ms (15.27%), Space: 42.5 MB (48.24%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,69 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         
-        Map<Character,Integer> s1map = new HashMap<>();
-        Map<Character,Integer> s2map = new HashMap<>();
-        
-        int k = s1.length();
-        
-        for(int i=0;i<k;i++){
-            char ch = s1.charAt(i);
-            s1map.put(ch,s1map.getOrDefault(ch,0)+1);
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1map, s2map))
+                return true;
         }
-        
-        int start =0;
-        int end =0;
-        
-        while(end<s2.length()){
-            
-            char ch = s2.charAt(end);
-            
-            
-                s2map.put(ch,s2map.getOrDefault(ch,0)+1);
-                
-            
-            
-            if(end-start+1<k){
-                end++;
-            }
-            else if(end-start+1==k){
-               
-                if(match(s1map,s2map))
-                    return true;
-                
-                 char chs = s2.charAt(start);
-            
-            if(s2map.containsKey(chs)){
-                s2map.put(chs,s2map.getOrDefault(chs,0)-1);
-                
-                if(s2map.get(chs)==0){
-                    s2map.remove(chs);
-                }
-                
-            }
-
-                start++;
-                end++;
-            }
-        }
-        
         return false;
     }
-    
-    public boolean match(Map<Character,Integer> map1,Map<Character,Integer> map2 ){
-        
-        if(map1.size()!=map2.size()){
-            return false;
+     public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
         }
-        
-        else{
-            
-            return map1.equals(map2);
-            
-//             return map1.entrySet().stream()
-//       .allMatch(e -> e.getValue().equals(map2.get(e.getKey())));
-            
-            
-        }
+        return true;
     }
 }
