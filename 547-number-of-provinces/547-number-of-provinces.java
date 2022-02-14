c61d42f5a7e7282a52a5f7e33d5d6c71b17cdb1d
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-02-14 20:44:23 +0530

    Time: 5 ms (27.81%), Space: 54.6 MB (5.06%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        
+        ArrayList<Edge>[] graph = new ArrayList[isConnected.length];
+        
+        for(int i=0; i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        
+        
+        for(int i=0;i<graph.length;i++){
+            for(int v=0;v<graph.length;v++){
+                
+                if(isConnected[i][v]==1){
+                graph[i].add(new Edge(i,v));
+                graph[i].add(new Edge(v,i));
+                }
+            }
+        }
+        
+        boolean[] visited = new boolean[graph.length];
+        int count =0;
+        for(int v=0;v<graph.length;v++){
+            if(!visited[v]){
+                util(graph,v,visited);
+                count++;
+            }
+        }
+        
+        return count;
+        
+        
+    }
+    
+    public void util(ArrayList<Edge>[] graph,int src,boolean[] visited ){
+        
+        
+        visited[src]=true;
+        for(Edge edge : graph[src]){
+            if(!visited[edge.nbr]){
+                util(graph,edge.nbr,visited);
+            }
+        }
+    }
+    
+    
+    
+}
+
+class Edge{
+    int src;
+    int nbr;
+    
+    public Edge(int src, int nbr){
+        this.src=src;
+        this.nbr=nbr;
+        
+    }
+}
