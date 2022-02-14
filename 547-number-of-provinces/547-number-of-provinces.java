54be5e483c9179817142e30de7b3e489cccd5d69
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-02-14 22:24:20 +0530

    Time: 1 ms (90.69%), Space: 53.2 MB (20.80%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,59 +1,29 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+     
+        int count=0;
+        boolean[] visited = new boolean[isConnected.length];
         
-        ArrayList<Edge>[] graph = new ArrayList[isConnected.length];
-        
-        for(int i=0; i<graph.length;i++){
-            graph[i]=new ArrayList<>();
-        }
-        
-        
-        for(int i=0;i<graph.length;i++){
-            for(int v=0;v<graph.length;v++){
-                
-                if(isConnected[i][v]==1){
-                graph[i].add(new Edge(i,v));
-                graph[v].add(new Edge(v,i));
-                }
-            }
-        }
-        
-        boolean[] visited = new boolean[graph.length];
-        int count =0;
-        for(int v=0;v<graph.length;v++){
-            if(!visited[v]){
-                util(graph,v,visited);
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                util(isConnected,i,visited);
                 count++;
             }
         }
         
         return count;
         
-        
     }
-    
-    public void util(ArrayList<Edge>[] graph,int src,boolean[] visited ){
         
-        
-        visited[src]=true;
-        for(Edge edge : graph[src]){
-            if(!visited[edge.nbr]){
-                util(graph,edge.nbr,visited);
+        public void util(int[][] graph, int src, boolean[] visited){
+            
+            
+            visited[src]=true;
+            for(int i=0;i<graph.length;i++){
+                if(!visited[i] && graph[src][i]==1){
+                    util(graph,i,visited);
+                }
             }
         }
-    }
     
-    
-    
-}
-
-class Edge{
-    int src;
-    int nbr;
-    
-    public Edge(int src, int nbr){
-        this.src=src;
-        this.nbr=nbr;
-        
-    }
 }
