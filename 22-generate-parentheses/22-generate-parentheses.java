c53275de543dd63ae6892f35165684c0352f4881
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-03-12 21:56:24 +0530

    Time: 3 ms (42.81%), Space: 44 MB (27.35%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,35 +1,24 @@
 class Solution {
-    
     public List<String> generateParenthesis(int n) {
-        
-        
         List<String> ans = new ArrayList<>();
         
-        util(n,0,0,new StringBuilder(),ans);
-        
+        util(ans,"",0,0,n);
         return ans;
     }
     
-    public void util(int n, int o,int c, StringBuilder str, List<String> ans){
-        if(str.length()==n*2){
-            ans.add(str.toString());
+    public void util(List<String> ans, String s, int o, int c, int n){
+        
+        if(s.length()==2*n){
+            ans.add(s);
             return;
         }
         
-        
-        
-        if(o < n){
-            str.append("(");
-            util(n,o+1,c,str,ans);
-            str.deleteCharAt(str.length()-1);
-            
-        }
-         if(c<o){
-             str.append(")");
-            util(n,o,c+1,str,ans);
-            str.deleteCharAt(str.length()-1);
+        if(o<n){
+            util(ans,s+"(",o+1,c,n);
         }
         
-        
+        if(c<o){
+            util(ans,s+")",o,c+1,n);
+        }
     }
 }
