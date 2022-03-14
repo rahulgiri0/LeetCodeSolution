f1e08c02a930bf9e24e8c08ae54ea87c0260f2be
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-03-14 21:03:41 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (73.11%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,19 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-       return uniquePathsHelper(m - 1, n - 1, new int[n][m]);
-  }
-  
-  private int uniquePathsHelper(int m, int n, int[][] memo) {
-    if (m < 0 || n < 0) {
-      return 0;
-    } else if (m == 0 || n == 0) {
-      return 1;
-    } else if (memo[n][m] > 0) {
-      return memo[n][m];
-    } else {
-      memo[n][m] = uniquePathsHelper(m - 1, n, memo) + uniquePathsHelper(m, n - 1, memo);
-      return memo[n][m];
+        return util(m-1,n-1, new int[n+1][m+1]);
     }
-  }
+    
+    public int util(int m, int n, int[][] memo){
+       if (m < 0 || n < 0) {
+      return 0;
+    }  if (m == 0 || n == 0) {
+      return 1;
+    }  if (memo[n][m] > 0) {
+      return memo[n][m];
+    } 
+      memo[n][m] = util(m - 1, n, memo) + util(m, n - 1, memo);
+      return memo[n][m];
+    
+    }
 }
+
+
