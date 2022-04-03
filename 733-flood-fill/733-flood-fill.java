e86e947193adff16fb47062a89c7226528192010
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-04-03 20:50:05 +0530

    Time: 1 ms (85.44%), Space: 48.4 MB (24.27%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,40 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        
-        
-       boolean[][] isvisited= new boolean[image.length][image[0].length];
         int key = image[sr][sc];
+        boolean[][] isVisited = new boolean[image.length][image[0].length];
         
-        return fill(image,sr,sc,newColor,isvisited,key);
-        
-        
+        floodFillUtil(image,sr,sc,newColor,key,isVisited);
+        return image;
     }
     
-    
-    public int[][] fill(int[][] image, int sr, int sc, int newColor, boolean[][] isvisited,int key){
-         if(sr<0 || sr>=image.length || sc<0 || sc >= image[0].length || image[sr][sc]!=key || isvisited[sr][sc]==true){
-            return image;
-        }
+    public void floodFillUtil(int[][] image, int sr, int sc, int newColor, int key,boolean[][] isVisited ){
+        
+       if(sr<0 || sr>=image.length || sc<0 || sc >= image[0].length || image[sr][sc]!=key || isVisited[sr][sc]==true)
+            return ;
         
        if(image[sr][sc]==key){
 
            image[sr][sc]=newColor;
        }
-        
-        isvisited[sr][sc]=true;
-        fill( image,  sr-1, sc,  newColor, isvisited,key);
-        fill( image,  sr, sc-1,  newColor,isvisited,key);
-        fill( image,  sr+1, sc,  newColor,isvisited,key);
-        fill( image,  sr, sc+1,  newColor,isvisited,key);
-        
-      
+        isVisited[sr][sc]=true;
+        floodFillUtil(image, sr,sc-1,newColor,key,isVisited);
+        floodFillUtil(image, sr+1,sc,newColor,key,isVisited);
+        floodFillUtil(image, sr,sc+1,newColor,key,isVisited);
+        floodFillUtil(image, sr-1,sc,newColor,key,isVisited);
         
         
-        
-        
-        return image;
+       
     }
-    
-    
-    
 }
