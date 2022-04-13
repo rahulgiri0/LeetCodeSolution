820d99ff857fee17f8a6fb58402042068462cb2c
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-04-13 20:54:36 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (90.73%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int val =1;
+        int[][] ans = new int[n][n];
+        
+        int minRow = 0;
+		int maxRow = n - 1;
+		int minCol = 0;
+		int maxCol = n - 1;
+
+		int nel = n*n;
+		int count = 0;
+        
+        while(count<nel){
+        for(int i=minRow;i<=maxRow && count < nel;i++){
+            ans[minRow][i]=val;
+            val++;
+            count++;
+        }
+        minRow++;
+        for(int i=minRow ; i<=maxRow && count<nel;i++){
+            ans[i][maxCol]=val;
+            val++;
+            count++;
+        }
+        maxCol--;
+        
+        for(int i=maxCol;i>=minCol && count<nel;i--){
+            ans[maxRow][i]=val;
+            val++;
+            count++;
+        }
+        
+        maxRow--;
+        
+        for(int i=maxRow;i>=minRow && count<nel;i--){
+            ans[i][minCol]=val;
+            val++;
+            count++;
+        }
+            minCol++;
+        }
+        
+        
+        return ans;
+        
+    }
+}
