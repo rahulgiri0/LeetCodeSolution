c4402b6b6cca849ce78d1e48512ec770a78ff62f
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-05-09 21:27:15 +0530

    Time: 1 ms (91.35%), Space: 42.8 MB (48.74%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,33 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
+       
+        
+        List<String> ans = new ArrayList<>();
+         if(digits.isEmpty())
+            return ans;
+        int idx = 0;
+        String[] map =  new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        util(digits,new StringBuilder(""),idx,map,ans);
+        return ans;
+		
+    }
+    
+    public void util(String digits,StringBuilder output, int idx , String[] map,List<String> ans){
+        
+        if(idx>=digits.length()){
+            String outStr = output.toString();
+            ans.add(outStr);
+            return;
+        }
+        
+        int num = digits.charAt(idx) -'0';
+        String keyValue = map[num];
+        
+        for(int i=0;i<keyValue.length();i++){
+            output = output.append(keyValue.charAt(i));
+            util(digits,output,idx+1,map,ans);
+           output.deleteCharAt(output.length()-1);
+        }
+        
     }
 }
