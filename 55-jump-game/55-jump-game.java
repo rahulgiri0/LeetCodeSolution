e07882c48ea29befc2c1d4e8abaa3139a8094d67
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-07-17 19:03:19 +0530

    Time: 694 ms (9.71%), Space: 68.4 MB (31.48%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-         
         int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return canJump(nums,0 ,dp);
+        Arrays.fill(dp, -1);
+        return canJump(nums,0,dp);
     }
     
-    public boolean canJump(int[] nums, int idx,int[] dp){
+    public boolean canJump(int[] nums, int idx, int[] dp){
         
         if(idx>=nums.length-1)
             return true;
@@ -15,20 +14,24 @@ class Solution {
             dp[idx] =0;
             return false;
         }
-       if(dp[idx]!=-1)
-           return dp[idx]==1;
         
-        int jump = nums[idx];
-       
-        for(int i =1; i<=jump;i++){
-            if(canJump(nums,idx+i,dp)){
-            dp[i]=1;
-                return true;
-            }
-          
+        
+        if(dp[idx]!=-1){
+            return dp[idx] == 1;
         }
         
-        dp[idx] =0;
+        
+        int jump = nums[idx];
+        
+        for(int i = 1; i<=jump; i++){
+            
+            if(canJump(nums, idx+i,dp)){
+                dp[idx] = 1;
+                return true;
+            }
+        }
+        
+        dp[idx] = 0;
         return false;
     }
 }
