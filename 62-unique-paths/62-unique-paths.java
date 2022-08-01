d108db52788acebd4db5fdb682f3f3e6cd91c430
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-08-01 20:16:19 +0530

    Time: 1 ms (55.39%), Space: 41 MB (40.73%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,43 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+       // return uniquePaths(0,0,m,n);
         
-        return util(m-1,n-1, new int[n+1][m+1]);
+        int[][] memo = new int[m+1][n+1];
+        
+        return uniquePaths(0,0,m,n,memo);
     }
     
-    public int util(int m, int n, int[][] memo){
-       if (m < 0 || n < 0) {
-      return 0;
-    }  if (m == 0 && n == 0) {
-      return 1;
-    }  if (memo[n][m] > 0) {
-      return memo[n][m];
-    } 
-      memo[n][m] = util(m - 1, n, memo) + util(m, n - 1, memo);
-      return memo[n][m];
+    // recursion
+    public int uniquePaths(int  i, int j, int m, int n){
+        
+        if(i<0 || i>m-1 || j<0 || j>n-1)
+            return 0;
+        
+        if(i == m-1 && j == n-1)
+            return 1;
+        
+        int right = uniquePaths(i,j+1,m,n);
+        int down = uniquePaths(i+1,j,m,n);
+        
+        return right+down;
+    }
     
+     public int uniquePaths(int  i, int j, int m, int n, int[][] memo){
+        
+        if(i<0 || i>m-1 || j<0 || j>n-1)
+            return 0;
+        
+         if(memo[i][j]!=0)
+             return memo[i][j];
+         
+        if(i == m-1 && j == n-1)
+            return 1;
+        
+        int right = uniquePaths(i,j+1,m,n,memo);
+        int down = uniquePaths(i+1,j,m,n,memo);
+        
+         memo[i][j] = right+down;
+        return memo[i][j];
     }
 }
-
 
