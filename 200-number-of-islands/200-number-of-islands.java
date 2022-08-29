3871d4d43a4ea65f1207edeb7e9fc0665ab060fd
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-08-29 20:50:16 +0530

    Time: 6 ms (39.74%), Space: 58.1 MB (18.46%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,36 +1,37 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int count=0;
         
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+        int count =0;
+        for(int i =0; i<grid.length ;i++)
+        {
+            for(int j =0; j<grid[i].length ;j++){
                 
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    getIsland(grid,i,j,visited);
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    util(i,j,grid,visited);
                     count++;
                 }
+                
             }
         }
         
         return count;
     }
     
-    public void getIsland(char[][] grid, int i, int j, boolean[][] visited){
+    
+    public void util(int src, int des,char[][] grid,  boolean[][] visited){
         
-        if(i<0 || j<0 || i>=grid.length || j>= grid[i].length || visited[i][j] || grid[i][j]=='0'){
-            return;
-        }
+        if(src<0 || des<0 || src>grid.length-1 || des> grid[0].length-1 || grid[src][des]=='0' || visited[src][des])
+            return ;
         
+        visited[src][des] = true;
         
-        visited[i][j]=true;
-        getIsland(grid,i-1,j,visited);
-        getIsland(grid,i,j-1,visited);
-        getIsland(grid,i,j+1,visited);
-        getIsland(grid,i+1,j,visited);
+         util(src,des+1,grid,visited);
+         util(src+1,des,grid,visited);
+         util(src-1,des,grid,visited);
+         util(src,des-1,grid,visited);
+        
         
         
     }
-
 }
