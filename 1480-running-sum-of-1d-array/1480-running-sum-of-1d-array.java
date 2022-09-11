b2390ac9563db5c9d2fee1693368e1dee0b307fe
rahulgiri0 <89238277+rahulgiri0@users.noreply.github.com>
2022-09-11 19:05:17 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (82.07%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-    int n= nums.length;
         
-        int[] arr = new int[n];
+        int preSum = nums[0];
         
-        for(int i = 0; i<nums.length; i++){
-            int sum=0;
-            for(int j=0 ; j <= i; j++){
-                sum=sum+nums[j];
-            }
-            arr[i]=sum;
+        for(int i =1; i<nums.length; i++){
+            nums[i] +=preSum;
+            preSum = nums[i];
         }
-        return arr;
+        
+        return nums;
     }
 }
